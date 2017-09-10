6111770d936bbed291ea91c8f4f7042e86088e64
Yaroslav <yaroslavtereshchuk@gmail.com>
2017-09-11 00:45:34 +0300

    qwerty

--- Encryptor.js
+++ Encryptor.js
@@ -1,13 +1,49 @@
-function CaesarCipher(){
-    var inputText = document.getElementById("messageId").value
-    var shift = Number(document.getElementById("keyId").value)
-    var charCode = 0
-    var outputText = ""
-    for(var index = 0; index < inputText.length; index++){
-        charCode = (inputText.charCodeAt(index) + shift)
-        outputText += String.fromCharCode(charCode)
-        console.log(charCode)
-        console.log(String.fromCharCode(charCode))
+const coeficients = {
+    begin: 97, 
+    end: 123,
+    limit: 26 
+};
+
+const mod = (a, b) => (a % b + b) % b;
+const shift = (charCode, key) => mod(charCode + key, coeficients.limit) + coeficients.begin;
+const isChar = (charCode) => charCode >= coeficients.begin &&
+    charCode <= coeficients.end ? true : false;
+
+function getAlphabet(alphabet) {
+    for(let i = coeficients.begin; i < coeficients.end; i++) {
+        alphabet.push(String.fromCharCode(i));
     }
-    document.getElementById("encrypterMessageId").value = outputText
+    return alphabet;
+}
+
+function convertIntoIntArray(line) {
+    const length = line.length;
+    let numbers = [];
+
+    for(let i = 0; i < length; i++) {
+        console.log(line);
+        if(isChar(line.charCodeAt(i))) 
+            numbers.push(Number(line.charCodeAt(i)));
+        else 
+            return [Number(line)];
+    }
+
+    return numbers;
+}
+
+function encrypt() {
+    let alphabet = getAlphabet([]);
+    let message = document.getElementById("messageId").value;
+    let keys = convertIntoIntArray(document.getElementById("keyId").value);
+    let encryptedMessage = "";
+    let j = 0;
+    const length = message.length;
+
+    for(let i = 0; i < length; i++) {
+        let currentKey = keys[mod(j++, keys.length)];
+        charCode = shift(message.charCodeAt(i), currentKey);
+        encryptedMessage += String.fromCharCode(charCode);
+    }
+
+    document.getElementById("encrypterMessageId").value = encryptedMessage;
 }
